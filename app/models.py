41ed057096fe3f83f5690a43937bbe23589bd52a
Evance23 <evancebarracks01@gmail.com>
2021-10-03 00:26:18 +0300

    Adding models for users

--- app/models.py
+++ app/models.py
@@ -7,3 +7,14 @@ from datetime import datetime
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+class User (UserMixin, db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email =db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String(20),nullable=False, default='default.jpg')
+    hashed_password = db.Column(db.String(250)) 
+    bio = db.Column(db.String(255),default = 'My default Bio') 
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
