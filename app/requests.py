a7c1b0e98c85b9593c22fe2cba949f7dcdf06666
Evance23 <evancebarracks01@gmail.com>
2021-10-03 00:05:00 +0300

    Add method for random quotes

---
+++ app/requests.py
@@ -0,0 +1,10 @@
+import urllib.request,json
+import requests
+from .models import Quote 
+
+url = 'http://quotes.stormconsultancy.co.uk/random.json'
+def get_quotes():
+    response = requests.get(url).json()
+
+    random_quote = Quote(response.get("author"),response.get("quote"))
+    return random_quotes
